346f6cc7913d3b9950f329f1f73435f0831bbf1a
PRADEEP PAL <PRADEEPPAL0072@GMAIL.COM>
2021-06-21 17:03:34 -0500

    Initial commit

---
+++ src/main/python/DailyDataIngestAndRefine.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, StringType, IntegerType, TimestampType, DoubleType
+import configparser
+# Initializing Spark Session
+Spark = SparkSession.builder.appName("DataIngestAndRefine").master("local").getOrCreate()
+
+# Read the Configs
+config = configparser.ConfigParser()
+config.read(r'../projectconfigs/config.ini')
+inputLocation = config.get('paths', 'inputLocation')
+
+
+landingFileSchema = StructType([
+    StructField('Sale_ID', StringType(), True),
+    StructField('Product_ID', StringType(), True),
+    StructField('Quantity_Sold', IntegerType(), True),
+    StructField('Vendor_ID', StringType(), True),
+    StructField('Sale_Date', TimestampType(), True),
+    StructField('Sale_Amount', DoubleType(), True),
+    StructField('Sale_Currency', IntegerType(), True)
+])
+
+landingFileDF = Spark.read\
+    .schema(landingFileSchema)\
+    .option("delimiter", "|")\
+    .csv(inputLocation)
+
+landingFileDF.show()
